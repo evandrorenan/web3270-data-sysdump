de04e12fcc58138f3ae3d1501fd2765a0ac5135c
renan3006@hotmail.com <renan3006@hotmail.com>
2023-07-30 14:17:18 -0300

    Desabilitando funcoes para testes iniciais no app services

--- src/main/java/com/evandrorenan/web3270datasysdump/domain/model/BaseLocator.java
+++ src/main/java/com/evandrorenan/web3270datasysdump/domain/model/BaseLocator.java
@@ -1,13 +1,10 @@
 package com.evandrorenan.web3270datasysdump.domain.model;
 
 import lombok.Builder;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Builder
-@Document(collection = "sysdumps")
 public record BaseLocator(
-    ObjectId _id,
+    String _id,
     String address,
     String hexContent
 ) {}
--- src/main/java/com/evandrorenan/web3270datasysdump/presentation/SaveController.java
+++ src/main/java/com/evandrorenan/web3270datasysdump/presentation/SaveController.java
@@ -1,7 +1,6 @@
 package com.evandrorenan.web3270datasysdump.presentation;
 
 import com.evandrorenan.web3270datasysdump.domain.model.BaseLocator;
-import com.evandrorenan.web3270datasysdump.domain.usecase.ISaveBaseLocatorUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,19 +8,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class SaveController {
 
-    private ISaveBaseLocatorUseCase saveUseCase;
-
     @Autowired
-    public SaveController(ISaveBaseLocatorUseCase saveUseCase) {
-        this.saveUseCase = saveUseCase;
+    public SaveController() {
+
     }
 
     @PostMapping("/base-locators/raw-input")
     public ResponseEntity<List<BaseLocator>> saveBaseLocatorUseCase(@RequestBody String rawInput) throws Exception {
-        return ResponseEntity.ok().body(saveUseCase.run(rawInput));
+        return ResponseEntity.ok().body(List.of(BaseLocator.builder()
+                                                    .address("Address")
+                                                    .hexContent("HexContent")
+                                                    ._id(UUID.randomUUID().toString())
+                                                    .build()));
+        //return ResponseEntity.ok().body(saveUseCase.run(rawInput));
     }
 }
