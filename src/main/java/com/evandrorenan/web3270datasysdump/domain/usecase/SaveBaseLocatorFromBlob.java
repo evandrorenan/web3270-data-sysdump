eedc179938b2e529a44f8604efc0f49624100aac
renan3006@hotmail.com <renan3006@hotmail.com>
2023-09-10 18:29:16 -0300

    Implementing base locator from blob (wip)

--- src/main/java/com/evandrorenan/web3270datasysdump/domain/usecase/SaveBaseLocatorFromBlob.java
+++ src/main/java/com/evandrorenan/web3270datasysdump/domain/usecase/SaveBaseLocatorFromBlob.java
@@ -1,2 +1,8 @@
-package com.evandrorenan.web3270datasysdump.domain.usecase;public interface SaveBaseLocatorFromBlob {
+package com.evandrorenan.web3270datasysdump.domain.usecase;
+
+import com.evandrorenan.web3270datasysdump.domain.model.BaseLocator;
+import java.util.List;
+
+public interface SaveBaseLocatorFromBlob {
+    List<BaseLocator> run(String blobId) throws Exception;
 }
--- src/main/java/com/evandrorenan/web3270datasysdump/domain/usecase/impl/SaveBaseLocatorFromBlobImpl.java
+++ src/main/java/com/evandrorenan/web3270datasysdump/domain/usecase/impl/SaveBaseLocatorFromBlobImpl.java
@@ -1,2 +1,13 @@
-package com.evandrorenan.web3270datasysdump.domain.usecase.impl;public class SaveBaseLocatorFromBlobImpl {
+package com.evandrorenan.web3270datasysdump.domain.usecase.impl;
+
+import com.evandrorenan.web3270datasysdump.domain.model.BaseLocator;
+import com.evandrorenan.web3270datasysdump.domain.usecase.SaveBaseLocatorFromBlob;
+
+import java.util.List;
+
+public class SaveBaseLocatorFromBlobImpl implements SaveBaseLocatorFromBlob {
+    @Override
+    public List<BaseLocator> run(String blobId) throws Exception {
+        return null;
+    }
 }
