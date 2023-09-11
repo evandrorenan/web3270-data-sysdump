3338244493df4924fa0a0de8f7040edac6682531
renan3006@hotmail.com <renan3006@hotmail.com>
2023-09-11 13:25:33 -0300

    Tranformacao de classe POJO em record

--- src/main/java/com/evandrorenan/web3270datasysdump/repository/util/BlobChunckHolder.java
+++ src/main/java/com/evandrorenan/web3270datasysdump/repository/util/BlobChunckHolder.java
@@ -1,11 +1,9 @@
 package com.evandrorenan.web3270datasysdump.repository.util;
 
 import lombok.Builder;
-import lombok.Data;
 
-@Data
 @Builder
-public class  BlobChunckHolder {
-    private final String chunckData;
-    private final String leftOverData;
-}
+public record BlobChunckHolder(
+    String chunckData,
+    String leftOverData
+) {}
